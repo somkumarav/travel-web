6dfc93f4a31d9d213811834d28eb4389f86d7cf1
somkumarav <somkumarav@gmail.com>
2025-06-09 19:13:20 +0530

    style: improved responsiveness

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,22 +12,28 @@ export const SiteHeader = () => {
             alt='Zenvia Logo'
             width={100}
             height={100}
-            className='h-[75px] w-auto logo-img-correction'
+            className='h-[75px] w-auto'
           />
         </Link>
         <nav className='ml-auto flex gap-6'>
-          <Link href='/packages' className='text-sm font-medium relative group'>
+          <Link
+            href='/packages'
+            className='text-xs md:text-sm font-medium relative group'
+          >
             Packages
             <span className='absolute left-0 right-0 bottom-0 h-0.5 bg-linear-to-r from-brand-green to-brand-blue scale-x-0 group-hover:scale-x-100 transition-transform duration-300'></span>
           </Link>
-          <Link href='/about' className='text-sm font-medium relative group'>
+          <Link
+            href='/about'
+            className='text-xs md:text-sm font-medium relative group'
+          >
             About Us
             <span className='absolute left-0 right-0 bottom-0 h-0.5 bg-linear-to-r from-brand-green to-brand-blue scale-x-0 group-hover:scale-x-100 transition-transform duration-300'></span>
           </Link>
           <Link
             target='_blank'
             href={LINKS.WHATSAPP}
-            className='text-sm font-medium relative group'
+            className='text-xs md:text-sm font-medium relative group'
           >
             Contact
             <span className='absolute left-0 right-0 bottom-0 h-0.5 bg-linear-to-r from-brand-green to-brand-blue scale-x-0 group-hover:scale-x-100 transition-transform duration-300'></span>
